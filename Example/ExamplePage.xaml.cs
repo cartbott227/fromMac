0039afe22a6e81830a84f4a5d0c01c2238b39a6c
cartbott227 <cartbott227@ed.amdsb.ca>
2017-03-08 14:23:01 -0500

    Initial check-in of module Example

---
+++ Example/ExamplePage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using AudioManager;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Example
+
+{
+	public partial class ExamplePage : ContentPage
+	{
+		//Creates the Stopwatch
+		Stopwatch sw = new Stopwatch();
+
+		public ExamplePage()
+		{
+			InitializeComponent();
+		}
+
+		async public void start_Clicked(object sender, EventArgs e)
+		{
+			await Audio.Manager.PlaySound("noot_noot.mp3");
+
+			//Starts the Stopwatch
+			sw.Start();
+
+			timeLabel.TextColor = Color.Green;
+
+
+			while (sw.IsRunning)
+			{
+				timeLabel.Text = sw.Elapsed + "";
+				await Task.Delay(10);
+			}
+
+		}
+
+		async public void stop_Clicked(object sender, EventArgs e)
+		{
+			await Audio.Manager.PlaySound("sound2.mp3");
+
+			//Pauses the Stopwatch
+			sw.Stop();
+
+			timeLabel.TextColor = Color.Red;
+		}
+
+		async public void reset_Clicked(object sender, EventArgs e)
+		{
+			await Audio.Manager.PlaySound("noot_noot.mp3");
+
+			//Resets the Stopwatch to 0 seconds
+			sw.Reset();
+
+			timeLabel.TextColor = Color.Black;
+			timeLabel.Text = "0";
+		}
+
+	}
+}
---
+++ Example/MyPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using AudioManager;
+using System.Diagnostics;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+
+//Program created by Carter Bott
+//on March 1st, 2017.
+//Description: A basic stop watch with the ability to time laps.
+namespace Example
+{
+	public partial class MyPage : ContentPage
+	{
+		//Creates the Stopwatch
+		Stopwatch sw = new Stopwatch();
+
+		public MyPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void start_Clicked(object sender, EventArgs e)
+		{
+			await Audio.Manager.PlaySound("noot_noot.mp3");
+
+			//Starts the Stopwatch
+			sw.Start();
+
+			//timeLabel.TextColor = Color.Green;
+
+
+			while (sw.IsRunning)
+			{
+				//timeLabel.Text = sw.ElapsedMilliseconds + "";
+				await Task.Delay(10);
+			}
+
+		}
+		private async void stop_Clicked(object sender, EventArgs e)
+		{
+			await Audio.Manager.PlaySound("sound2.mp3");
+
+			//Pauses the Stopwatch
+			sw.Stop();
+
+			//timeLabel.TextColor = Color.Red;
+		}
+
+		private async void reset_Clicked(object sender, EventArgs e)
+		{ 
+			await Audio.Manager.PlaySound("noot_noot.mp3");
+
+			//Resets the Stopwatch to 0 seconds
+			sw.Reset();
+
+			//timeLabel.TextColor(Color.Black);
+			//timeLabel.Text = "0";
+
+		}
+
+
+
+
+
+	}
+}
