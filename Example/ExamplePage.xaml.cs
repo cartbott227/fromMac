4be602ac562f1889e07ea994f9d1590f3dea6f1e
cartbott227 <cartbott227@ed.amdsb.ca>
2017-03-10 14:10:18 -0500

    Final Program

--- Example/ExamplePage.xaml.cs
+++ Example/ExamplePage.xaml.cs
@@ -5,6 +5,10 @@ using AudioManager;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
+//Program created by Carter Bott
+//on March 6th, 2017
+//Description: A basic stopwatch that allows you to start, stop, and clear the current time.
+
 namespace Example
 
 {
@@ -13,6 +17,9 @@ namespace Example
 		//Creates the Stopwatch
 		Stopwatch sw = new Stopwatch();
 
+		//Stores current time before second button click
+		TimeSpan interval = TimeSpan.Zero;
+			
 		public ExamplePage()
 		{
 			InitializeComponent();
@@ -20,41 +27,55 @@ namespace Example
 
 		async public void start_Clicked(object sender, EventArgs e)
 		{
-			await Audio.Manager.PlaySound("noot_noot.mp3");
+			await Audio.Manager.PlaySound("button.mp3");
 
 			//Starts the Stopwatch
 			sw.Start();
 
+			//Sets the time label colour to green
 			timeLabel.TextColor = Color.Green;
 
+			//Sets lapTime equal to the current time minus the time since the start button was pressed
+			TimeSpan lapTime = sw.Elapsed - interval;
+
+			//Displays previous lap time to the lap label
+			lapLabel.Text = "Previous Lap: " + lapTime;
+
+			//Sets the interval equal to the current time
+			interval = sw.Elapsed;
 
 			while (sw.IsRunning)
 			{
+				//Displays the elapsed time to the time label and waits 10ms
 				timeLabel.Text = sw.Elapsed + "";
-				await Task.Delay(10);
+				await Task.Delay(10);  
 			}
 
 		}
 
 		async public void stop_Clicked(object sender, EventArgs e)
 		{
-			await Audio.Manager.PlaySound("sound2.mp3");
+			await Audio.Manager.PlaySound("button.mp3");
 
 			//Pauses the Stopwatch
-			sw.Stop();
+			sw.Stop(); 
 
+			//Sets the time label colour to red
 			timeLabel.TextColor = Color.Red;
 		}
 
 		async public void reset_Clicked(object sender, EventArgs e)
 		{
-			await Audio.Manager.PlaySound("noot_noot.mp3");
+			await Audio.Manager.PlaySound("button.mp3");
 
 			//Resets the Stopwatch to 0 seconds
 			sw.Reset();
 
+			//Sets the time label colour to red
 			timeLabel.TextColor = Color.Black;
-			timeLabel.Text = "0";
+
+			//Changes the time label to show zero elapsed time
+			timeLabel.Text = "00.00.00.000";
 		}
 
 	}
